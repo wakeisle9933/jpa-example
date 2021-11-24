815f66eb892ed664c2f1cd53422ac3ce150c1034
Wakestand <wakeisle9933@gmail.com>
2021-11-24 17:07:12 +0900

    주석 추가

--- jpa-example/src/main/java/com/jpa/jpaexample/controller/CrudController.java
+++ jpa-example/src/main/java/com/jpa/jpaexample/controller/CrudController.java
@@ -27,31 +27,37 @@ public class CrudController {
     @PersistenceContext
     private EntityManager entityManager;
 
+    // 전체 조회
     @GetMapping("searchAll")
     public List<CrudEntity> searchAll() {
         return crudService.searchAll();
     }
 
+    // age 파라미터를 사용한 조회
     @GetMapping("searchParam")
     public List<CrudEntity> searchParam(@RequestParam(value = "age") int age) {
         return crudService.searchParam(age);
     }
 
+    // Repository에 작성한 쿼리를 이용한 조회
     @GetMapping("searchParamRepo")
     public List<CrudEntity> searchParamRepo(@RequestParam(value = "name") String name) {
         return crudService.searchParamRepo(name);
     }
 
+    // 삽입
     @GetMapping("insert")
     public String insertMember(@RequestParam(value = "name") String name, @RequestParam(value = "age") int age) {
         return crudService.insertMember(name, age);
     }
 
+    // 수정
     @GetMapping("update")
     public String updateMember(@RequestParam(value = "name") String name, @RequestParam(value = "age") int age) {
         return crudService.updateMember(name, age);
     }
 
+    // 삭제
     @GetMapping("delete")
     public String deleteMember(@RequestParam(value = "name") String name) {
         return crudService.deleteMember(name);
